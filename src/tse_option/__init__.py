de89886a8d8a0b5619627ebb0884180739d74ca0
Seyed Mohammad Sokout <80194917+sm-sokout@users.noreply.github.com>
2023-07-20 17:50:44 +0330

    0.1.2.1

--- src/tse_option/__init__.py
+++ src/tse_option/__init__.py
@@ -101,7 +101,7 @@ def stock_price(stock_name):
 #----------------------------------------
 
 def tse_options(symbol, stock=True):
-    url = "https://tse.ir/json/MarketWatch/MarketWatch_7.xml"
+    url = "https://old.tse.ir/json/MarketWatch/MarketWatch_7.xml"
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     data = pd.read_html(url)[0]
     data = data[data["نماد"].notna()]
