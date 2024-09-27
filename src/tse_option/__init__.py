155bbaf221957d461749772a86de002bd7355c55
Mohammad Sokout <80194917+sm-sokout@users.noreply.github.com>
2024-09-27 11:46:03 +0330

    0.1.2.3

--- src/tse_option/__init__.py
+++ src/tse_option/__init__.py
@@ -15,31 +15,29 @@ def BSM_call(S0, K, rf, T, sigma):
     """
     Black, Scholes and Merton model for option pricing
     """
-
     d1 = (np.log(S0/K) + (rf + (sigma ** 2 )/2) * T)/(sigma * (T ** 0.5))
     d2 = d1 - (sigma * (T ** 0.5))
-    call_price = (S0 * norm.cdf(d1)) - (K * np.e ** (-rf*T) * norm.cdf(d2))
+    call_price = (S0 * norm.cdf(d1)) - (K * np.exp(-rf*T) * norm.cdf(d2))
     return call_price
 
 #----------------------------------------
 
-def bs_vega(S0, K, rf, T, Sigma):
+def Vega(S0, K, rf, T, Sigma):
     d1 = (np.log(S0 / K) + (rf + 0.5 * Sigma ** 2) * T) / (Sigma * np.sqrt(T))
     return S0 * norm.pdf(d1) * np.sqrt(T)
 
 #----------------------------------------
 
-
 def find_IV(market_price, S0, K, rf, T, *args):
     """
-    Calculation of Impolied Volatility
+    Calculation of Implied Volatility
     """
     MAX_ITERATIONS = 200
     PRECISION = 1.0e-4
     Sigma = 0.5
     for i in range(0, MAX_ITERATIONS):
         price = BSM_call(S0, K, rf, T, Sigma)
-        vega = bs_vega(S0, K, rf, T, Sigma)
+        vega = Vega(S0, K, rf, T, Sigma)
         diff = market_price - price  # our root
         if (abs(diff) < PRECISION):
             return Sigma
@@ -52,35 +50,25 @@ def risk_free_interest_rate():
     """"
     Calculation of risk-free interest rate based on the yield of treasury bills
     """
-    url = "https://ifb.ir/ytm.aspx"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    fopen = rq.get(url, headers=headers).content
-    html = pd.read_html(io.StringIO(fopen.decode("utf-8")), header=0, index_col=0)
-    html = pd.DataFrame(html[1])
-    html = html["YTM"].replace({"سررسید شده":np.nan})
-    html = html.dropna().reset_index(drop=True)
-    ytm = []
-    for i in html:
-        if len(i.split("%")[0].split("/")) == 1:
-            ytm.append(int(i.split("%")[0].split("/")[0])/100)
-        elif len(i.split("%")[0].split("/")[0])==1:
-            ret = i.split("%")[0].split("/")[0] + i.split("%")[0].split("/")[1]
-            ytm.append(int(ret)/10**(len(list(ret))+1)) 
-        else:
-            ret = i.split("%")[0].split("/")[0] + i.split("%")[0].split("/")[1]
-            ytm.append(int(ret)/10**(len(list(ret))))          
-            
-    return np.mean(ytm)
+    pd.options.mode.copy_on_write = True 
+    url ='https://ifb.ir/ytm.aspx'
+    df = pd.read_html(url, encoding='utf-8')[1]
+    df = df[df['YTM'] != 'سررسید شده']
+    df['YTM'] = df['YTM'].str.replace('/', '.')
+    df['YTM'] = df['YTM'].str.rstrip('%')
+    df['YTM'] = df['YTM'].astype(float)
+    rf = df['YTM'].mean()/100
+    return rf
 
 #----------------------------------------
 
-def stock_id(stock_name):
+def stock_id(symbol):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    url = f"http://old.tsetmc.com/tsev2/data/search.aspx?skey={stock_name}"
+    url = f"http://old.tsetmc.com/tsev2/data/search.aspx?skey={symbol}"
     html = rq.get(url, headers=headers).text
     info = html.split(";")
     for i in info:
-        if i.split(",")[0] == stock_name:
+        if i.split(",")[0] == symbol:
             return i.split(",")[2]
         else:
             try:
@@ -90,8 +78,8 @@ def stock_id(stock_name):
 
 #----------------------------------------
 
-def stock_price(stock_name):
-    id = stock_id(stock_name)
+def stock_price(symbol):
+    id = stock_id(symbol)
     if id is None:
         raise NameError("This symbol does not exist. Please enter the symbol correctly!")
     url = "http://old.tsetmc.com/tsev2/data/instinfofast.aspx?i="+id+"&c=34"
@@ -113,7 +101,7 @@ def tse_options(symbol, stock=True):
     return data
 
 #----------------------------------------
-
+'''
 def farabourse_options(symbol, stock=True):
     url = "https://ifb.ir/OptionStockQuote.aspx"
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -128,11 +116,11 @@ def farabourse_options(symbol, stock=True):
     "قیمت.1": "حجم بهترین سفارش خرید","قیمت":"قیمت بهترین سفارش فروش",
     "تعداد موقعیت\u200cهای باز":"موقیعت های باز"})
     return data
-
+'''
 #----------------------------------------
 
 
-def download_history(symbols, j_date=False, start=None, end=None, adjust_price=False, drop_unadjusted=False):
+def download(symbols, j_date=False, start=None, end=None, adjust_price=False, drop_unadjusted=False):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     if type(symbols) == str:
         symbols = [symbols]
@@ -196,22 +184,26 @@ def download_history(symbols, j_date=False, start=None, end=None, adjust_price=F
 
 #----------------------------------------
 
-
-def pricing_based_on_stock(stock_name:str, trading_days:int=100, IV=False, leverage=True, P_BSM=False, sort="Maturity"):
+def option_chain(symbol:str, trading_days:int=100, IV=False, leverage=True, P_BSM=False, sort="Maturity"):
     """
     Valuation of all options on a particular stock
     """
-    stock_name = stock_name.replace("ی","ي").replace('ک','ك')
-    last_price = stock_price(stock_name)
-    df_stock = download_history(stock_name, start=None, end=None, adjust_price=True)[-trading_days:]
+    symbol = symbol.replace("ی","ي").replace('ک','ك')
+    last_price = stock_price(symbol)
+    df_stock = download(symbol, start=None, end=None, adjust_price=True)[-trading_days:]
     df = df_stock[["Adj Close"]].copy()
     df["return"] = np.log(df["Adj Close"]/df["Adj Close"].shift())
     sigma = df["return"].std() * np.sqrt(240)
-    rf = risk_free_interest_rate()
-    data = tse_options(stock_name)
+    try:
+        rf = risk_free_interest_rate()
+    except:
+        print('در حال حاضر دریافت اطلاعات از فرابورس امکان پذیر نیست.\nلطفا نرخ بهره بدون ریسک مورد نظر را بصورت یک عدد اعشاری وارد کنید.')
+        rf = float(input('... '))
+    data = tse_options(symbol)
     if data.empty:
-        data = farabourse_options(stock_name)
-
+        #data = farabourse_options(symbol)
+        raise NameError('در حال حاضر دریافت اطلاعات از فرابورس امکان پذیر نیست.')
+    data.reset_index(inplace=True, drop=True)
     col = ["Symbol", "Strike Price", "Status", "Maturity(day)","Open Interest", "Bid", "Ask", "Delta"]
     if IV == True:
         col.append("IV")
@@ -220,22 +212,22 @@ def pricing_based_on_stock(stock_name:str, trading_days:int=100, IV=False, lever
     col.append("BSM")
     if P_BSM == True:
         col.append("%Price/BSM")
-    df = pd.DataFrame(index=range(1,1+len(data)),columns=col)
+    df = pd.DataFrame(index=range(len(data)),columns=col)
 
     for i in range(len(data)):
-        df.iloc[i]["Symbol"] = data.iloc[i]["نماد"]
-        df.iloc[i]["Stock"] = stock_name
-        df.iloc[i]["Strike Price"] = int(data.iloc[i]["قیمت اعمال"])
-        if last_price > 1.025*df.iloc[i]["Strike Price"]:
-            df.iloc[i]["Status"] = "ITM"
-        elif last_price < 0.975*df.iloc[i]["Strike Price"]:
-            df.iloc[i]["Status"] = "OTM"
+        df.loc[i,'Symbol'] = data.loc[i,"نماد"]
+        df.loc[i,"Stock"] = symbol
+        df.loc[i,"Strike Price"] = int(data.loc[i,"قیمت اعمال"])
+        if last_price > 1.025*df.loc[i,"Strike Price"]:
+            df.loc[i,'Status'] = "ITM"
+        elif last_price < 0.975*df.loc[i,"Strike Price"]:
+            df.loc[i,'Status'] = "OTM"
         else:
-            df.iloc[i]["Status"] = "ATM"
-        df.iloc[i]["Maturity(day)"] = int(data.iloc[i]["روزهای باقیمانده تا سررسید"])
-        df.iloc[i]["Open Interest"] = data.iloc[i]["موقیعت های باز"]
-        df.iloc[i]["Bid"] = data.iloc[i]["حجم بهترین سفارش خرید"]
-        df.iloc[i]["Ask"] = data.iloc[i]["قیمت بهترین سفارش فروش"]
+            df.loc[i,'Status'] = "ATM"
+        df.loc[i,"Maturity(day)"] = int(data.loc[i,"روزهای باقیمانده تا سررسید"])
+        df.loc[i,'Open Interest'] = data.loc[i,"موقیعت های باز"]
+        df.loc[i,'Bid'] = data.loc[i,"حجم بهترین سفارش خرید"]
+        df.loc[i,'Ask'] = data.loc[i,"قیمت بهترین سفارش فروش"]
  
 
     if sort.upper() == "MATURITY":
@@ -245,41 +237,41 @@ def pricing_based_on_stock(stock_name:str, trading_days:int=100, IV=False, lever
     else:
         df = df.sort_values(by=['Open Interest'], ascending=False)
     df = df.reset_index(drop=True)
-
+    #return df
     for i in range(len(df)):
-        K = df["Strike Price"].iloc[i]
-        T = df["Maturity(day)"].iloc[i]/360
+        K = df.loc[i,"Strike Price"]
+        T = df.loc[i,"Maturity(day)"]/360
         d1 = (np.log(last_price/K) + (rf + (sigma ** 2 )/2) * T)/(sigma * (T ** 0.5))
         d2 = d1 - (sigma * (T ** 0.5))
-        df["BSM"].iloc[i] = round((last_price * norm.cdf(d1)) - (K * np.e ** (-rf*T) * norm.cdf(d2)))
-        market_price = int(df["Ask"].iloc[i])
-        df["Delta"].iloc[i] = round(norm.cdf(d1),2)
+        df.loc[i, 'BSM'] = round((last_price * norm.cdf(d1)) - (K * np.exp(-rf*T) * norm.cdf(d2)))
+        market_price = int(df.loc[i,'Ask'])
+        df.loc[i,'Delta'] = round(norm.cdf(d1),2)
         if IV == True:
             if market_price == 0:
-                df["IV"].iloc[i] = "%-"
+                df.loc[i,'IV'] = "%-"
             else:
-                df["IV"].iloc[i] = f"%{round(find_IV(market_price, last_price, K, rf, T)*100,1)}"
-                if df["IV"].iloc[i]=='%nan':
-                    df["IV"].iloc[i] = "%-"
+                df.loc[i,'IV'] = f"%{round(find_IV(market_price, last_price, K, rf, T)*100,1)}"
+                if df.loc[i,'IV']=='%nan':
+                    df.loc[i,'IV'] = "%-"
             
 
-        if leverage == True:    
+        if leverage == True:
             try:
-                df["Leverage"].iloc[i] = round(df["Delta"].iloc[i] * last_price / df["Ask"].iloc[i], 2)
+                df.loc[i,'Leverage'] = round(df.loc[i,'Delta'] * last_price / df.loc[i,'Ask'], 2)
             except:
-                df["Leverage"].iloc[i] = np.inf
+                df.loc[i,'Leverage'] = np.inf
         if P_BSM == True:
             try:
-                price_bsm = round(-100*(market_price/df.iloc[i]["BSM"]-1),1)
+                price_bsm = round(-100*(market_price/df.loc[i,"BSM"]-1),1)
                 
                 if price_bsm >=0:
-                    df["%Price/BSM"].iloc[i] = f"%{price_bsm}\U0001f7e2"
+                    df.loc[i,'%Price/BSM'] = f"%{price_bsm}\U0001f7e2"
                 else:
-                    df["%Price/BSM"].iloc[i] = f"%{price_bsm}\U0001F534"
+                    df.loc[i,'%Price/BSM'] = f"%{price_bsm}\U0001F534"
                 if market_price == 0:
-                    df["%Price/BSM"].iloc[i] = "%-"
+                    df.loc[i,'%Price/BSM'] = "%-"
             except:
-                df["%Price/BSM"].iloc[i] = "%-"
+                df.loc[i,'%Price/BSM'] = "%-"
 
 
     df.index = df["Symbol"]
@@ -289,30 +281,31 @@ def pricing_based_on_stock(stock_name:str, trading_days:int=100, IV=False, lever
 
 #----------------------------------------
 
-def pricing_based_on_option(option_name:str, trading_days:int=100, IV=False, leverage=True, P_BSM=False):
+def call(option_symbol:str, trading_days:int=100, IV=False, leverage=True, P_BSM=False):
     """
     Valuation of a particular option
     """
-    option_name = option_name.replace("ی","ي").replace('ک','ك')
+    option_symbol = option_symbol.replace("ی","ي").replace('ک','ك')
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    url = f"http://old.tsetmc.com/tsev2/data/search.aspx?skey={option_name}"
+    url = f"http://old.tsetmc.com/tsev2/data/search.aspx?skey={option_symbol}"
     html = rq.get(url, headers=headers).text
     if len(html.split(";")) == 2:
-        stock_name = html.split(",")[1].split("-")[0].split(" ")[1]
-        last_price = stock_price(stock_name)
-        df_stock = download_history(stock_name, start=None, end=None, adjust_price=True)[-trading_days:]
+        stock_symbol = html.split(",")[1].split("-")[0].split(" ")[1]
+        last_price = stock_price(stock_symbol)
+        df_stock = download(stock_symbol, start=None, end=None, adjust_price=True)[-trading_days:]
         df = df_stock[["Adj Close"]].copy()
         df["return"] = np.log(df["Adj Close"]/df["Adj Close"].shift())
         sigma = df["return"].std() * np.sqrt(240)
         rf = risk_free_interest_rate()
-        option_in_tse = option_name + "1"
+        option_in_tse = option_symbol + "1"
         data = tse_options(option_in_tse, False)
         if data.empty:
-            data = farabourse_options(option_name, False)
-        
+            #data = farabourse_options(option_symbol, False)
+            data = pd.DataFrame()
+        data.reset_index(inplace=True, drop=True)
         if data.empty:
             raise NameError("This symbol does not exist or has expired. Please enter the symbol correctly!")
-
+        
         else:
             col = ["Symbol", "Strike Price", "Status", "Maturity(day)","Open Interest", "Bid", "Ask", "Delta"]
             if IV == True:
@@ -322,55 +315,55 @@ def pricing_based_on_option(option_name:str, trading_days:int=100, IV=False, lev
             col.append("BSM")
             if P_BSM == True:
                 col.append("%Price/BSM")
-            df = pd.DataFrame(index=range(1,1+len(data)),columns=col)
+            df = pd.DataFrame(index=range(len(data)),columns=col)
 
-            df.iloc[0]["Symbol"] = data.iloc[0]["نماد"]
-            df.iloc[0]["Stock"] = stock_name
-            df.iloc[0]["Strike Price"] = data.iloc[0]["قیمت اعمال"]
-            if last_price > 1.025*df.iloc[0]["Strike Price"]:
-                df.iloc[0]["Status"] = "ITM"
-            elif last_price < 0.975*df.iloc[0]["Strike Price"]:
-                df.iloc[0]["Status"] = "OTM"
+            df.loc[0,'Symbol'] = data.loc[0,"نماد"]
+            df.loc[0,'Stock'] = stock_symbol
+            df.loc[0,'Strike Price'] = data.loc[0,"قیمت اعمال"]
+            if last_price > 1.025*df.loc[0,"Strike Price"]:
+                df.loc[0,"Status"] = "ITM"
+            elif last_price < 0.975*df.loc[0,"Strike Price"]:
+                df.loc[0,"Status"] = "OTM"
             else:
-                df.iloc[0]["Status"] = "ATM"
-            df.iloc[0]["Maturity(day)"] = int(data.iloc[0]["روزهای باقیمانده تا سررسید"])
-            df.iloc[0]["Open Interest"] = data.iloc[0]["موقیعت های باز"]
-            df.iloc[0]["Bid"] = data.iloc[0]["حجم بهترین سفارش خرید"]
-            df.iloc[0]["Ask"] = data.iloc[0]["قیمت بهترین سفارش فروش"]
+                df.loc[0,"Status"] = "ATM"
+            df.loc[0,"Maturity(day)"] = int(data.loc[0,"روزهای باقیمانده تا سررسید"])
+            df.loc[0,"Open Interest"] = data.loc[0,"موقیعت های باز"]
+            df.loc[0,"Bid"] = data.loc[0,"حجم بهترین سفارش خرید"]
+            df.loc[0,"Ask"] = data.loc[0,"قیمت بهترین سفارش فروش"]
 
             df = df.reset_index(drop=True)
 
-            K = df["Strike Price"].iloc[0]
-            T = df["Maturity(day)"].iloc[0]/360
+            K = df.loc[0,"Strike Price"]
+            T = df.loc[0,"Maturity(day)"]/360
             d1 = (np.log(last_price/K) + (rf + (sigma ** 2 )/2) * T)/(sigma * (T ** 0.5))
             d2 = d1 - (sigma * (T ** 0.5))
-            df["BSM"].iloc[0] = round((last_price * norm.cdf(d1)) - (K * np.e ** (-rf*T) * norm.cdf(d2)))
-            market_price = int(df["Ask"].iloc[0])
-            df["Delta"].iloc[0] = round(norm.cdf(d1),2)
+            df.loc[0,"BSM"] = round((last_price * norm.cdf(d1)) - (K * np.e ** (-rf*T) * norm.cdf(d2)))
+            market_price = int(df.loc[0,"Ask"])
+            df.loc[0,"Delta"] = round(norm.cdf(d1),2)
             if IV == True:
                 if market_price == 0:
-                    df["IV"].iloc[0] = "%-"
+                    df.loc[0,"IV"] = "%-"
                 else:
-                    df["IV"].iloc[0] = f"%{round(find_IV(market_price, last_price, K, rf, T)*100,1)}"
-                    if df["IV"].iloc[0]=='%nan':
-                        df["IV"].iloc[0] = "%-"
+                    df.loc[0,"IV"] = f"%{round(find_IV(market_price, last_price, K, rf, T)*100,1)}"
+                    if df.loc[0,"IV"] == '%nan':
+                        df.loc[0,"IV"] = "%-"
             if leverage == True:
                 try:
-                    df["Leverage"].iloc[0] = round(df["Delta"].iloc[0] * last_price /market_price, 2)
+                    df.loc[0,"Leverage"] = round(df.loc[0,"Delta"] * last_price /market_price, 2)
                 except:
-                    df["Leverage"].iloc[0] = np.inf
+                    df.loc[0,"Leverage"] = np.inf
 
             if P_BSM == True:
                 try:
-                    price_bsm = round(-100*(market_price/df.iloc[0]["BSM"]-1),1)
+                    price_bsm = round(-100*(market_price/df.loc[0,"BSM"]-1),1)
                     if price_bsm >=0:
-                        df["%Price/BSM"].iloc[0] = f"%{price_bsm}\U0001f7e2"
+                        df.loc[0,"%Price/BSM"] = f"%{price_bsm}\U0001f7e2"
                     else:
-                        df["%Price/BSM"].iloc[0] = f"%{price_bsm}\U0001F534"
+                        df.loc[0,"%Price/BSM"] = f"%{price_bsm}\U0001F534"
                     if market_price == 0:
-                        df["%Price/BSM"].iloc[0] = "%-"
+                        df.loc[0,"%Price/BSM"] = "%-"
                 except:
-                    df["%Price/BSM"].iloc[0] = "%-"
+                    df.loc[0,"%Price/BSM"] = "%-"
 
             df.index = df["Symbol"]
             df = df.drop("Symbol", axis=1)
@@ -391,38 +384,3 @@ def pricing_based_on_option(option_name:str, trading_days:int=100, IV=False, lev
 
 
 
-
-
-
-
-
-
-
-
-
-
-"""""
-def pricing_based_on_option(option_name:str, trading_days:int=100, IV=False):
-
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    url = f"http://www.tsetmc.com/tsev2/data/search.aspx?skey={option_name}"
-    html = rq.get(url, headers=headers).text
-    if len(html.split(";")) == 2:
-        stock_name = html.split(",")[1].split("-")[0].split(" ")[1]
-        df = pricing_based_on_stock(stock_name, trading_days, IV)
-        try:
-            df = pd.DataFrame(df.loc[f"{option_name}1"])
-        except KeyError:
-            try:
-                df = pd.DataFrame(df.loc[option_name])
-            except KeyError:
-                print("This symbol does not exist. Please enter the symbol correctly!")
-
-        return df
-    elif len(html.split(";")) == 1:
-        print("This symbol does not exist. Please enter the symbol correctly!")
-    else:
-        print("Please enter the symbol completely!")
-
-
-"""
